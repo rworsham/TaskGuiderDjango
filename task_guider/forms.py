8c4e06de65cdae1e7e04c7ae8ebc9f030bca958f
rworsham <robertworsham01@gmail.com>
2024-07-02 23:51:31 -0400

    added html from Workflo the flask version of this and outlined forms needed

---
+++ task_guider/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+
+
+class TaskForm(forms.Form):
+    pass
+
+
+class TaskEditForm(forms.Form):
+    pass
+
+
+class LoginForm(forms.Form):
+    pass
+
+
+class RegistrationForm(forms.Form):
+    pass
+
+
+class CommentForm(forms.Form):
+    pass
+
+
+class ProjectForm(forms.Form):
+    pass
+
+
+class WorkStateCreateForm(forms.Form):
+    pass
+
+
+class WorkStateChangeFrom(forms.Form):
+    pass
+
+
+class WorkStateEditForm(forms.Form):
+    pass
+
+
+class SubscribeUser(forms.Form):
+    pass
+
+
+class UnsubscribeUserForm(forms.Form):
+    pass
+
