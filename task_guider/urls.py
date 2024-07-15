756adc0c9611e710016752ac9472c6fdf6b0710f
rworsham <robertworsham01@gmail.com>
2024-07-14 23:34:48 -0400

    working further on transition from jinja to DTL

--- task_guider/urls.py
+++ task_guider/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path("events", views.events, name="events"),
     path("login", views.login, name="login"),
     path("overview", views.overview, name="overview"),
-    path("project_view", views.project_view, name="project_view"),
+    path("project_view/<int:id>/", views.project_view, name="project_view"),
     path("projects", views.projects, name="projects"),
     path("register", views.register, name="register"),
     path("settings", views.settings, name="settings"),
-    path("task", views.task, name="task"),
+    path("task/<int:id>/", views.task, name="task"),
     path("calendar", views.calendar, name="calendar"),
     path("logout", views.logout, name="logout"),
 ]
